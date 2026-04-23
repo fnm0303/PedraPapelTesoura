237551aee75ea080de836eeb98da6c068add089c
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-23 19:52:48 -0300

    Logica sequencial feita

--- PedraPapelTesoura.ConsoleApp/Program.cs
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -1,6 +1,10 @@
-﻿const int ESCOLHA_PEDRA = 1; //constantes tudo maiusculo
+﻿using System.Security.Cryptography;
+
+const int ESCOLHA_PEDRA = 1; //constantes tudo maiusculo
 const int ESCOLHA_PAPEL = 2;
 const int ESCOLHA_TESOURA = 3;
+
+int escolhaJogador;
 
 do
 {
@@ -14,10 +18,60 @@ do
     Console.WriteLine("-------------------------");
 
     Console.Write("Escolha uma opção válida: ");
-    string escolhaJogador = Console.ReadLine();
+    string? strEscolhaJogador = Console.ReadLine(); //? para permitir que a variável seja null
 
-    if (escolhaJogador != "1" && escolhaJogador != "2" && escolhaJogador != "3")
+    if (strEscolhaJogador == "1" || strEscolhaJogador == "2" || strEscolhaJogador == "3")
     {
-
+        escolhaJogador = Convert.ToInt32(strEscolhaJogador);
+        break;
     }
 } while (true);
+
+int escolhaComputador = RandomNumberGenerator.GetInt32(1, 4); //nro aleatório entre 1, 2 ou 3
+
+if (escolhaJogador == escolhaComputador)
+{
+    Console.WriteLine("Empate!");
+}
+else if (escolhaJogador == ESCOLHA_PEDRA)
+{
+    Console.Write("Pedra vs");
+    if (escolhaComputador == ESCOLHA_PAPEL)
+    {
+        Console.WriteLine("Papel");
+        Console.WriteLine("O computador venceu!");
+    }
+    else if (escolhaComputador == ESCOLHA_TESOURA)
+    {
+        Console.WriteLine("Tesoura");
+        Console.WriteLine("Você venceu!");
+    }
+}
+else if (escolhaJogador == ESCOLHA_PAPEL)
+{
+    Console.Write("Papel vs");
+    if (escolhaComputador == ESCOLHA_TESOURA)
+    {
+        Console.WriteLine("Tesoura");
+        Console.WriteLine("O computador venceu!");
+    }
+    else if (escolhaComputador == ESCOLHA_PEDRA)
+    {
+        Console.WriteLine("Tesoura");
+        Console.WriteLine("Você venceu!");
+    }
+}
+else if (escolhaJogador == ESCOLHA_TESOURA)
+{
+    Console.Write("Tesoura vs");
+    if (escolhaComputador == ESCOLHA_PEDRA)
+    {
+        Console.WriteLine("Pedra");
+        Console.WriteLine("O computador venceu!");
+    }
+    else if (escolhaComputador == ESCOLHA_PAPEL)
+    {
+        Console.WriteLine("Papel");
+        Console.WriteLine("Você venceu!");
+    }
+}
