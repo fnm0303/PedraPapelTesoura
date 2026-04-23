6f9255b3f65d2d98b9e7691a20335ec537324ef9
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-23 20:31:07 -0300

    Adicionados métodos padrão estruturado

--- PedraPapelTesoura.ConsoleApp/Program.cs
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -1,90 +1,116 @@
 ﻿using System.Security.Cryptography;
 
-const int ESCOLHA_PEDRA = 1; //constantes tudo maiusculo
-const int ESCOLHA_PAPEL = 2;
-const int ESCOLHA_TESOURA = 3;
-
-while (true)
+class Program
 {
-    int escolhaJogador;
-
-    do
+    static void Main()
     {
-        Console.Clear();
-        Console.WriteLine("-------------------------");
-        Console.WriteLine("Pedra, Papel, Tesoura");
-        Console.WriteLine("-------------------------");
-        Console.WriteLine("1 - Pedra");
-        Console.WriteLine("2 - Papel");
-        Console.WriteLine("3 - Tesoura");
-        Console.WriteLine("-------------------------");
-
-        Console.Write("Escolha uma opção válida: ");
-        string? strEscolhaJogador = Console.ReadLine(); //? para permitir que a variável seja null
-
-        Console.WriteLine("-------------------------");
-
-        if (strEscolhaJogador == "1" || strEscolhaJogador == "2" || strEscolhaJogador == "3")
+        while (true)
         {
-            escolhaJogador = Convert.ToInt32(strEscolhaJogador);
-            break;
+            int escolhaJogador = ObterEscolhaJogador();
+
+            int escolhaComputador = ObterEscolhaComputador();
+
+            CompararEscolhas(escolhaJogador, escolhaComputador);
+
+            Console.WriteLine("-------------------------");
+            Console.Write("Deseja continuar? s/n ");
+            string? opcaoContinuar = Console.ReadLine().ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
         }
-    } while (true);
 
-    int escolhaComputador = RandomNumberGenerator.GetInt32(1, 4); //nro aleatório entre 1, 2 ou 3
-
-    if (escolhaJogador == escolhaComputador)
-    {
-        Console.WriteLine("Empate!");
     }
-    else if (escolhaJogador == ESCOLHA_PEDRA)
+    static int ObterEscolhaJogador()
     {
-        Console.Write("Pedra vs");
-        if (escolhaComputador == ESCOLHA_PAPEL)
+        int escolhaJogador;
+
+        do
         {
-            Console.WriteLine("Papel");
-            Console.WriteLine("O computador venceu!");
-        }
-        else if (escolhaComputador == ESCOLHA_TESOURA)
-        {
-            Console.WriteLine("Tesoura");
-            Console.WriteLine("Você venceu!");
-        }
-    }
-    else if (escolhaJogador == ESCOLHA_PAPEL)
-    {
-        Console.Write("Papel vs");
-        if (escolhaComputador == ESCOLHA_TESOURA)
-        {
-            Console.WriteLine("Tesoura");
-            Console.WriteLine("O computador venceu!");
-        }
-        else if (escolhaComputador == ESCOLHA_PEDRA)
-        {
-            Console.WriteLine("Tesoura");
-            Console.WriteLine("Você venceu!");
-        }
-    }
-    else if (escolhaJogador == ESCOLHA_TESOURA)
-    {
-        Console.Write("Tesoura vs");
-        if (escolhaComputador == ESCOLHA_PEDRA)
-        {
-            Console.WriteLine("Pedra");
-            Console.WriteLine("O computador venceu!");
-        }
-        else if (escolhaComputador == ESCOLHA_PAPEL)
-        {
-            Console.WriteLine("Papel");
-            Console.WriteLine("Você venceu!");
-        }
+            Console.Clear();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Pedra, Papel, Tesoura");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1 - Pedra");
+            Console.WriteLine("2 - Papel");
+            Console.WriteLine("3 - Tesoura");
+            Console.WriteLine("-------------------------");
+
+            Console.Write("Escolha uma opção válida: ");
+            string? strEscolhaJogador = Console.ReadLine(); //? para permitir que a variável seja null
+
+            Console.WriteLine("-------------------------");
+
+            if (strEscolhaJogador == "1" || strEscolhaJogador == "2" || strEscolhaJogador == "3")
+            {
+                escolhaJogador = Convert.ToInt32(strEscolhaJogador);
+                break;
+            }
+
+        } while (true);
+
+        return escolhaJogador;
     }
 
-    Console.Write("Deseja continuar? s/n ");
-    string? opcaoContinuar = Console.ReadLine().ToUpper();
+    static int ObterEscolhaComputador()
+    {
+        return RandomNumberGenerator.GetInt32(1, 4);
+        //return escolhaComputador;
+    }
 
-    Console.WriteLine("-------------------------");
+    static void CompararEscolhas(int escolhaJogador, int escolhaComputador)
+    {
+        const int ESCOLHA_PEDRA = 1; //constantes tudo maiusculo
+        const int ESCOLHA_PAPEL = 2;
+        const int ESCOLHA_TESOURA = 3;
 
-    if (opcaoContinuar != "S")
-        break;
+        if (escolhaJogador == escolhaComputador)
+        {
+            Console.WriteLine("Empate!");
+        }
+        else if (escolhaJogador == ESCOLHA_PEDRA)
+        {
+            Console.Write("Pedra vs");
+            if (escolhaComputador == ESCOLHA_PAPEL)
+            {
+                Console.WriteLine(" Papel");
+                Console.WriteLine("O computador venceu!");
+            }
+            else if (escolhaComputador == ESCOLHA_TESOURA)
+            {
+                Console.WriteLine(" Tesoura");
+                Console.WriteLine("Você venceu!");
+            }
+        }
+        else if (escolhaJogador == ESCOLHA_PAPEL)
+        {
+            Console.Write("Papel vs");
+            if (escolhaComputador == ESCOLHA_TESOURA)
+            {
+                Console.WriteLine(" Tesoura");
+                Console.WriteLine("O computador venceu!");
+            }
+            else if (escolhaComputador == ESCOLHA_PEDRA)
+            {
+                Console.WriteLine(" Tesoura");
+                Console.WriteLine("Você venceu!");
+            }
+        }
+        else if (escolhaJogador == ESCOLHA_TESOURA)
+        {
+            Console.Write("Tesoura vs");
+            if (escolhaComputador == ESCOLHA_PEDRA)
+            {
+                Console.WriteLine(" Pedra");
+                Console.WriteLine("O computador venceu!");
+            }
+            else if (escolhaComputador == ESCOLHA_PAPEL)
+            {
+                Console.WriteLine(" Papel");
+                Console.WriteLine("Você venceu!");
+            }
+        }
+    }
+
 }
+
